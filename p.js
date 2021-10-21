3af9602ddf2d586771c615bbb30fda4046031aeb
hopper01 <anand.dhawan01@gmail.com>
2021-10-21 16:34:20 +0530

    test

--- p.js
+++ p.js
@@ -7,26 +7,37 @@ h1.setAttribute("style", "color: red; font-size: 40px");
 //   console.log(csp);
 //   // does this exist? Is is any good?
 // });
-// document.addEventListener("securitypolicyviolation", (e) => {
-//     console.log(e);
-//   console.log(e.blockedURI);
-//   console.log(e.violatedDirective);
-//   console.log(e.originalPolicy);
-// });
 
+var report;
+document.addEventListener("securitypolicyviolation", (e) => {
+    console.log(e);
+    var report = 'CSP Violation Report: \n';
+    report = report + '\t' + `blockedURI: ${e.blockedURI}\t` + `violatedDirective: ${e.violatedDirective}\t\n` + `\toriginalPolicy: ${e.originalPolicy}`;
+    console.log(report);
+}, {once: true});
+console.log(report);
 //    var req = new XMLHttpRequest();
 //    req.open('GET', document.location, false);
+// //    req.setRequestHeader("Access-Control-Allow-Origin", "*");
+//     // req.open('GET', "https://developer.mozilla.org/en-US/docs/Web/API/SecurityPolicyViolationEvent");
+  
 //    req.send(null);
 //    var headers = req.getAllResponseHeaders().toLowerCase();
-// //    var header = req.getResponseHeader('last-modified');
+//    console.log(req.getAllResponseHeaders().includes('accept-ranges: bytes'));
+//    var header = req.getResponseHeader('content-security-policy');
 // //    alert(headers);
+// console.log(header);
 // console.log(headers);
 
+function nothing() {
+    console.log("nothing");
+}
 // function reqListener () {
-//   console.log(this.responseText);
+//    var header = req.getResponseHeader('last-modified');
+//    console.log(object);
 // }
 
 // var oReq = new XMLHttpRequest();
 // oReq.addEventListener("load", reqListener);
-// oReq.open("GET", document.location);
+// oReq.open("GET", document.location, false);
 // oReq.send();
