3ddac751281d97aa910000ba93771e35201fcd81
hopper01 <anand.dhawan01@gmail.com>
2021-10-21 23:03:20 +0530

    test

--- p.js
+++ p.js
@@ -1,21 +1,31 @@
 var h1 = document.querySelector("h1");
 h1.setAttribute("style", "color: red; font-size: 40px");
+h1.addEventListener("click", (e) => {
+console.log(e);
+});
+
+
 // fetch(document.location.href)
 // .then(resp => {
 //     console.log(resp);
 //   const csp = resp.headers.get('Content-Security-Policy');
-//   console.log(csp);
+//   console.log(csp);  
 //   // does this exist? Is is any good?
 // });
 
-var report;
-document.addEventListener("securitypolicyviolation", (e) => {
-    console.log(e);
-    var report = 'CSP Violation Report: \n';
-    report = report + '\t' + `blockedURI: ${e.blockedURI}\t` + `violatedDirective: ${e.violatedDirective}\t\n` + `\toriginalPolicy: ${e.originalPolicy}`;
-    console.log(report);
-}, {once: true});
-console.log(report);
+// var report;
+// document.addEventListener("securitypolicyviolation", (e) => {
+//     console.log(e);
+//     var report = 'CSP Violation Report: \n';
+//     report = report + '\t' + `blockedURI: ${e.blockedURI}\t` + `violatedDirective: ${e.violatedDirective}\t\n` + `\toriginalPolicy: ${e.originalPolicy}`;
+//     console.log(report);
+// }, {once: true});
+// console.log(report);
+
+
+
+
+
 //    var req = new XMLHttpRequest();
 //    req.open('GET', document.location, false);
 // //    req.setRequestHeader("Access-Control-Allow-Origin", "*");
